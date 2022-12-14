8feee238f7e65253e0738522e9b6841e4a161ac3
Mohammed Chliyah <mchliyah@e2r2p8.1337.ma>
2022-12-14 19:54:00 +0100

    the firs SEGV start rendring

--- raycast/cub.h
+++ raycast/cub.h
@@ -6,7 +6,7 @@
 /*   By: mchliyah <mchliyah@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 16:27:59 by mchliyah          #+#    #+#             */
-/*   Updated: 2022/12/13 18:28:50 by mchliyah         ###   ########.fr       */
+/*   Updated: 2022/12/14 19:51:27 by mchliyah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,15 @@
 
 # define X 1000
 # define Y 800
+# define TILESIZE 64
+
+#define wall '1'
+#define empty '0'
+#define playr 'N'
+
+#define green 0x0000FF00
+#define red 0x00FF0000
+#define blue 0x000000FF
 
 char map[5][5] = {{'1','1','1','1','1'}
 				 ,{'1','0','0','0','1'}
@@ -34,6 +43,8 @@ char map[5][5] = {{'1','1','1','1','1'}
 
 typedef struct s_map
 {
+	const int rows;
+	const int cols;
 	char	**textr;
 	char	**colrs;
 	char	**map;
@@ -42,8 +53,8 @@ typedef struct s_map
 
 typedef struct	s_ax
 {
-	double	x;
-	double	y;
+	int	x;
+	int	y;
 }				t_ax;
 
 typedef struct s_player
@@ -62,8 +73,8 @@ typedef	struct	s_window
 	void	*win;
 	void	*img;
 	void	*img_adrs;
-	int		bpp;
-	int		len;
+	int		bits_per_pixel;
+	int		line_length;
 	int		size_line;
 	int		endian;
 	t_ax	size;
@@ -74,6 +85,7 @@ typedef struct s_cub
 {
 	t_window	window;
 	t_map		map;
+	t_player	player;
 }t_cub;
 
 //map handling
--- raycast/main.c
+++ raycast/main.c
@@ -6,17 +6,63 @@
 /*   By: mchliyah <mchliyah@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 16:27:39 by mchliyah          #+#    #+#             */
-/*   Updated: 2022/12/13 17:49:15 by mchliyah         ###   ########.fr       */
+/*   Updated: 2022/12/14 19:51:59 by mchliyah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include  "cub.h"
 
-// bool	init(t_cub *cub)
-// {
-// 	if (!windowinit())
-// 		exit_error("eroor windows ");
-// }
+void my_mlx_pixel_put(t_window *data, int x, int y, int color)
+{
+	char *dst;
+
+	dst = data->img_adrs + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+void render_Square(t_cub cub, t_ax *pos, char c, int color)
+{
+	int i = 0;
+	int j = 0;
+	while (i < TILESIZE)
+	{
+		j = 0;
+		while (j < TILESIZE)
+		{
+			if (c == wall)
+				my_mlx_pixel_put(&cub.window, (*pos).x * TILESIZE + j, (*pos).y * TILESIZE + i, color);
+			else if (c == playr)
+				my_mlx_pixel_put(&cub.window, (*pos).x * TILESIZE + j, (*pos).y * TILESIZE + i, color);
+			else
+				my_mlx_pixel_put(&cub.window, (*pos).x * TILESIZE + j, (*pos).y * TILESIZE + i, color);
+			j++;
+		}
+		i++;
+	}
+}
+
+void render(t_cub cub)
+{
+	t_ax pos;
+	
+	pos.x = 0;
+	pos.y = 0;
+	while (pos.y < Y)
+	{
+		pos.x = 0;
+		while (pos.x < X)
+		{
+			if (map[pos.y][pos.x] == wall)
+				render_Square(cub, &pos, wall, red);
+			else if (map[pos.y][pos.x] == 'N')
+				render_Square(cub, &pos, playr, blue);
+			else
+				render_Square(cub, &pos, empty, green);
+			pos.x++;
+		}
+		pos.y++;
+	}
+}
 
 int	key_press(int key, t_cub *cub)
 {
@@ -46,9 +92,9 @@ int	main(int ac, char **av)
 	cub.window.mlx = mlx_init();
 	cub.window.win = mlx_new_window(cub.window.mlx, X, Y, "cub3D");
 	cub.window.img = mlx_new_image(cub.window.mlx, X, Y);
-	cub.window.img_adrs = mlx_get_data_addr(cub.window.img, &cub.window.bpp, &cub.window.len, &cub.window.endian);
+	cub.window.img_adrs = mlx_get_data_addr(cub.window.img, &cub.window.bits_per_pixel, &cub.window.line_length, &cub.window.endian);
 	mlx_put_image_to_window(cub.window.mlx, cub.window.win, cub.window.img, 0, 0);
-	
+	render(cub);
 	mlx_hook(cub.window.win, 2, 0, key_press, &cub);
 	mlx_hook(cub.window.win, 17, 0, t_close, &cub);
 	mlx_loop(cub.window.mlx);
